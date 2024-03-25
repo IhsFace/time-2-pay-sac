98ea191f43ee73c69205691107dd96497cc5c0fd
IhsFace <ihsaanifzal75@gmail.com>
2024-03-25 13:13:34 +1100

    Update files

---
+++ time-2-pay-sac/time-2-pay-sac/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblTitle = New Label()
+        lblEmployeeName = New Label()
+        lblDepartment = New Label()
+        lblHoursWorked = New Label()
+        lblHourlyPayRate = New Label()
+        txtEmployeeName = New TextBox()
+        txtDepartment = New TextBox()
+        txtHoursWorked = New TextBox()
+        txtHourlyPayRate = New TextBox()
+        btnAdd = New Button()
+        btnDisplayAll = New Button()
+        btnPayReport = New Button()
+        btnSearch = New Button()
+        btnClear = New Button()
+        lstDisplay = New ListBox()
+        SuspendLayout()
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.AutoSize = True
+        lblTitle.Font = New Font("Arial", 16.125F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTitle.Location = New Point(176, 27)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(476, 51)
+        lblTitle.TabIndex = 0
+        lblTitle.Text = "PlantsRUs - Time2Pay"
+        ' 
+        ' lblEmployeeName
+        ' 
+        lblEmployeeName.AutoSize = True
+        lblEmployeeName.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblEmployeeName.Location = New Point(60, 131)
+        lblEmployeeName.Name = "lblEmployeeName"
+        lblEmployeeName.Size = New Size(246, 36)
+        lblEmployeeName.TabIndex = 1
+        lblEmployeeName.Text = "Employee Name"
+        ' 
+        ' lblDepartment
+        ' 
+        lblDepartment.AutoSize = True
+        lblDepartment.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblDepartment.Location = New Point(60, 207)
+        lblDepartment.Name = "lblDepartment"
+        lblDepartment.Size = New Size(180, 36)
+        lblDepartment.TabIndex = 2
+        lblDepartment.Text = "Department"
+        ' 
+        ' lblHoursWorked
+        ' 
+        lblHoursWorked.AutoSize = True
+        lblHoursWorked.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblHoursWorked.Location = New Point(60, 300)
+        lblHoursWorked.Name = "lblHoursWorked"
+        lblHoursWorked.Size = New Size(376, 36)
+        lblHoursWorked.TabIndex = 3
+        lblHoursWorked.Text = "Hours Worked This Week"
+        ' 
+        ' lblHourlyPayRate
+        ' 
+        lblHourlyPayRate.AutoSize = True
+        lblHourlyPayRate.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblHourlyPayRate.Location = New Point(60, 375)
+        lblHourlyPayRate.Name = "lblHourlyPayRate"
+        lblHourlyPayRate.Size = New Size(243, 36)
+        lblHourlyPayRate.TabIndex = 4
+        lblHourlyPayRate.Text = "Hourly Pay Rate"
+        ' 
+        ' txtEmployeeName
+        ' 
+        txtEmployeeName.Location = New Point(319, 128)
+        txtEmployeeName.Name = "txtEmployeeName"
+        txtEmployeeName.Size = New Size(374, 39)
+        txtEmployeeName.TabIndex = 5
+        ' 
+        ' txtDepartment
+        ' 
+        txtDepartment.Location = New Point(319, 204)
+        txtDepartment.Name = "txtDepartment"
+        txtDepartment.Size = New Size(374, 39)
+        txtDepartment.TabIndex = 6
+        ' 
+        ' txtHoursWorked
+        ' 
+        txtHoursWorked.Location = New Point(445, 297)
+        txtHoursWorked.Name = "txtHoursWorked"
+        txtHoursWorked.Size = New Size(248, 39)
+        txtHoursWorked.TabIndex = 7
+        ' 
+        ' txtHourlyPayRate
+        ' 
+        txtHourlyPayRate.Location = New Point(445, 375)
+        txtHourlyPayRate.Name = "txtHourlyPayRate"
+        txtHourlyPayRate.Size = New Size(248, 39)
+        txtHourlyPayRate.TabIndex = 8
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(60, 471)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(150, 75)
+        btnAdd.TabIndex = 9
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnDisplayAll
+        ' 
+        btnDisplayAll.Font = New Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnDisplayAll.Location = New Point(216, 471)
+        btnDisplayAll.Name = "btnDisplayAll"
+        btnDisplayAll.Size = New Size(150, 75)
+        btnDisplayAll.TabIndex = 10
+        btnDisplayAll.Text = "Display All"
+        btnDisplayAll.UseVisualStyleBackColor = True
+        ' 
+        ' btnPayReport
+        ' 
+        btnPayReport.Font = New Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnPayReport.Location = New Point(387, 471)
+        btnPayReport.Name = "btnPayReport"
+        btnPayReport.Size = New Size(150, 75)
+        btnPayReport.TabIndex = 11
+        btnPayReport.Text = "Pay Report"
+        btnPayReport.UseVisualStyleBackColor = True
+        ' 
+        ' btnSearch
+        ' 
+        btnSearch.Font = New Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnSearch.Location = New Point(543, 471)
+        btnSearch.Name = "btnSearch"
+        btnSearch.Size = New Size(150, 75)
+        btnSearch.TabIndex = 12
+        btnSearch.Text = "Search"
+        btnSearch.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnClear.Location = New Point(319, 871)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(150, 46)
+        btnClear.TabIndex = 13
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' lstDisplay
+        ' 
+        lstDisplay.BackColor = Color.Honeydew
+        lstDisplay.Font = New Font("Arial", 8F)
+        lstDisplay.FormattingEnabled = True
+        lstDisplay.ItemHeight = 24
+        lstDisplay.Location = New Point(12, 573)
+        lstDisplay.Name = "lstDisplay"
+        lstDisplay.Size = New Size(750, 268)
+        lstDisplay.TabIndex = 14
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.PaleGreen
+        ClientSize = New Size(774, 929)
+        Controls.Add(lstDisplay)
+        Controls.Add(btnClear)
+        Controls.Add(btnSearch)
+        Controls.Add(btnPayReport)
+        Controls.Add(btnDisplayAll)
+        Controls.Add(btnAdd)
+        Controls.Add(txtHourlyPayRate)
+        Controls.Add(txtHoursWorked)
+        Controls.Add(txtDepartment)
+        Controls.Add(txtEmployeeName)
+        Controls.Add(lblHourlyPayRate)
+        Controls.Add(lblHoursWorked)
+        Controls.Add(lblDepartment)
+        Controls.Add(lblEmployeeName)
+        Controls.Add(lblTitle)
+        Name = "Form1"
+        Text = "Time2Pay"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblEmployeeName As Label
+    Friend WithEvents lblDepartment As Label
+    Friend WithEvents lblHoursWorked As Label
+    Friend WithEvents lblHourlyPayRate As Label
+    Friend WithEvents txtEmployeeName As TextBox
+    Friend WithEvents txtDepartment As TextBox
+    Friend WithEvents txtHoursWorked As TextBox
+    Friend WithEvents txtHourlyPayRate As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDisplayAll As Button
+    Friend WithEvents btnPayReport As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstDisplay As ListBox
+
+End Class
---
+++ time-2-pay-sac/time-2-pay-sac/Form1.vb
@@ -0,0 +1,247 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim blnItemsValid As Boolean = True
+
+    Sub Display()
+        Dim strEmployeeName() As String
+        Dim strDepartment() As String
+        Dim strHoursWorked() As String
+        Dim strHourlyPayRate() As String
+        Dim Time2PayDataFile As StreamReader = File.OpenText("Time2PayData.txt")
+        Dim intItemCounter As Integer
+
+        lstDisplay.Items.Clear()
+
+        Do While Time2PayDataFile.Peek <> -1
+            Time2PayDataFile.ReadLine()
+            Time2PayDataFile.ReadLine()
+            Time2PayDataFile.ReadLine()
+            Time2PayDataFile.ReadLine()
+            intItemCounter = intItemCounter + 1
+        Loop
+
+        Time2PayDataFile.Close()
+
+        ReDim strEmployeeName(intItemCounter - 1)
+        ReDim strDepartment(intItemCounter - 1)
+        ReDim strHoursWorked(intItemCounter - 1)
+        ReDim strHourlyPayRate(intItemCounter - 1)
+        Dim Time2PayDataFile2 As StreamReader = File.OpenText("Time2PayData.txt")
+
+        For i = 0 To intItemCounter - 1
+            strEmployeeName(i) = Time2PayDataFile2.ReadLine()
+            strDepartment(i) = Time2PayDataFile2.ReadLine()
+            strHoursWorked(i) = Time2PayDataFile2.ReadLine()
+            strHourlyPayRate(i) = Time2PayDataFile2.ReadLine()
+            lstDisplay.Items.Add("Employee Name: " & strEmployeeName(i))
+            lstDisplay.Items.Add("Department: " & strDepartment(i))
+            lstDisplay.Items.Add("Hours Worked: " & strHoursWorked(i))
+            lstDisplay.Items.Add("Hourly Pay Rate: " & FormatCurrency(strHourlyPayRate(i)))
+        Next
+
+        Time2PayDataFile2.Close()
+    End Sub
+
+    Sub DoValidation()
+        If String.IsNullOrEmpty(txtEmployeeName.Text) Then
+            MessageBox.Show("Please enter an employee name.")
+            blnItemsValid = False
+            txtEmployeeName.Focus()
+            Return
+        ElseIf IsNumeric(txtEmployeeName.Text) = True Then
+            MessageBox.Show("The employee name should not include numbers.")
+            blnItemsValid = False
+            txtEmployeeName.Focus()
+            Return
+        ElseIf String.IsNullOrEmpty(txtDepartment.Text) Then
+            MessageBox.Show("Please enter a department name.")
+            blnItemsValid = False
+            txtDepartment.Focus()
+            Return
+        ElseIf IsNumeric(txtDepartment.Text) = True Then
+            MessageBox.Show("The department name should not include numbers.")
+            blnItemsValid = False
+            txtDepartment.Focus()
+            Return
+        ElseIf String.IsNullOrEmpty(txtHoursWorked.Text) Then
+            MessageBox.Show("Please enter the amount of hours worked.")
+            blnItemsValid = False
+            txtHoursWorked.Focus()
+            Return
+        ElseIf IsNumeric(txtHoursWorked.Text) = False Then
+            MessageBox.Show("The amount of hours worked should only include numbers.")
+            blnItemsValid = False
+            txtHoursWorked.Focus()
+            Return
+        ElseIf Val(txtHoursWorked.Text) < 1 Then
+            MessageBox.Show("The amount of hours worked should not be less than 1 hour.")
+            blnItemsValid = False
+            txtHoursWorked.Focus()
+            Return
+        ElseIf Val(txtHoursWorked.Text) > 60 Then
+            MessageBox.Show("The amount of hours worked should not be greater than 60 hours.")
+            blnItemsValid = False
+            txtHoursWorked.Focus()
+            Return
+        ElseIf String.IsNullOrEmpty(txtHourlyPayRate.Text) Then
+            MessageBox.Show("Please enter the hourly pay rate.")
+            blnItemsValid = False
+            txtHourlyPayRate.Focus()
+            Return
+        ElseIf IsNumeric(txtHourlyPayRate.Text) = False Then
+            MessageBox.Show("The hourly pay rate should only include numbers.")
+            blnItemsValid = False
+            txtHourlyPayRate.Focus()
+            Return
+        ElseIf Val(txtHourlyPayRate.Text) < 20 Then
+            MessageBox.Show("The hourly pay rate should not be less than $20.00.")
+            blnItemsValid = False
+            txtHourlyPayRate.Focus()
+            Return
+        ElseIf Val(txtHourlyPayRate.Text) > 100 Then
+            MessageBox.Show("The hourly pay rate should not be greater than $100.00.")
+            blnItemsValid = False
+            txtHourlyPayRate.Focus()
+            Return
+        Else
+            blnItemsValid = True
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        DoValidation()
+
+        If blnItemsValid Then
+            Dim strEmployeeName As String
+            Dim strDepartment As String
+            Dim dblHoursWorked As Double
+            Dim dblHourlyPayRate As Double
+            Dim Time2PayDataFile As StreamWriter = File.AppendText("Time2PayData.txt")
+
+            strEmployeeName = txtEmployeeName.Text
+            strDepartment = txtDepartment.Text
+            dblHoursWorked = txtHoursWorked.Text
+            dblHourlyPayRate = txtHourlyPayRate.Text
+            Time2PayDataFile.WriteLine(strEmployeeName)
+            Time2PayDataFile.WriteLine(strDepartment)
+            Time2PayDataFile.WriteLine(dblHoursWorked)
+            Time2PayDataFile.WriteLine(dblHourlyPayRate)
+
+            MessageBox.Show("Employee details have been successfully added to the textfile.")
+            Time2PayDataFile.Close()
+            Display()
+        End If
+    End Sub
+
+    Private Sub btnDisplayAll_Click(sender As Object, e As EventArgs) Handles btnDisplayAll.Click
+        Display()
+    End Sub
+
+    Private Sub btnPayReport_Click(sender As Object, e As EventArgs) Handles btnPayReport.Click
+        Dim strEmployeeName As String
+        Dim strDepartment As String
+        Dim strHoursWorked As Double
+        Dim strHourlyPayRate As Double
+        Dim dblTotalAmountDue As Double
+        Dim dblTotalOverallPay As Double
+        Dim dblTotalFinancePay As Double
+        Dim dblTotalSalesPay As Double
+        Dim dblTotalTechnologyPay As Double
+        Dim dblTotalLegalPay As Double
+        Dim dblTotalMarketingPay As Double
+        Dim dblHighestPay As Double
+        Dim strEmployeeHighestPay As String
+        Dim Time2PayDataFile As StreamReader = File.OpenText("Time2PayData.txt")
+
+        lstDisplay.Items.Clear()
+
+        Do While Time2PayDataFile.Peek <> -1
+            strEmployeeName = Time2PayDataFile.ReadLine()
+            strDepartment = Time2PayDataFile.ReadLine()
+            strHoursWorked = Time2PayDataFile.ReadLine()
+            strHourlyPayRate = Time2PayDataFile.ReadLine()
+
+            dblTotalAmountDue = Val(strHoursWorked) * Val(strHourlyPayRate)
+            dblTotalOverallPay = dblTotalOverallPay + dblTotalAmountDue
+
+            Select Case strDepartment
+                Case "Finance"
+                    dblTotalFinancePay = dblTotalFinancePay + dblTotalAmountDue
+                Case "Sales"
+                    dblTotalSalesPay = dblTotalSalesPay + dblTotalAmountDue
+                Case "Technology"
+                    dblTotalTechnologyPay = dblTotalTechnologyPay + dblTotalAmountDue
+                Case "Legal"
+                    dblTotalLegalPay = dblTotalLegalPay + dblTotalAmountDue
+                Case "Marketing"
+                    dblTotalMarketingPay = dblTotalMarketingPay + dblTotalAmountDue
+            End Select
+
+            If dblHighestPay < dblTotalAmountDue Then
+                dblHighestPay = dblTotalAmountDue
+                strEmployeeHighestPay = strEmployeeName
+            End If
+
+            lstDisplay.Items.Add("Employee Name: " & strEmployeeName & ", Pay Amount: " & FormatCurrency(dblTotalAmountDue))
+        Loop
+
+        lstDisplay.Items.Add("Overall Total Pay: " & FormatCurrency(dblTotalOverallPay))
+        lstDisplay.Items.Add("Finance Department Total Pay: " & FormatCurrency(dblTotalFinancePay))
+        lstDisplay.Items.Add("Sales Department Total Pay: " & FormatCurrency(dblTotalSalesPay))
+        lstDisplay.Items.Add("Technology Department Total Pay: " & FormatCurrency(dblTotalTechnologyPay))
+        lstDisplay.Items.Add("Legal Department Total Pay: " & FormatCurrency(dblTotalLegalPay))
+        lstDisplay.Items.Add("Marketing Department Total Pay: " & FormatCurrency(dblTotalMarketingPay))
+        lstDisplay.Items.Add("Employee with Highest Pay Name: " & strEmployeeHighestPay & ", Pay Amount: " & FormatCurrency(dblHighestPay))
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        If String.IsNullOrEmpty(txtEmployeeName.Text) Then
+            MessageBox.Show("Please enter an employee name.")
+            blnItemsValid = False
+            txtEmployeeName.Focus()
+            Return
+        ElseIf IsNumeric(txtEmployeeName.Text) = True Then
+            MessageBox.Show("The employee name should not include numbers.")
+            blnItemsValid = False
+            txtEmployeeName.Focus()
+            Return
+        End If
+
+        lstDisplay.Items.Clear()
+
+        Dim strEmployeeName As String
+        Dim strDepartment As String
+        Dim strHoursWorked As String
+        Dim strHourlyPayRate As String
+        Dim strEmployeeDisplay As String = txtEmployeeName.Text
+        Dim Time2PayDataFile As StreamReader = File.OpenText("Time2PayData.txt")
+
+        Do While Time2PayDataFile.Peek <> -1
+            strEmployeeName = Time2PayDataFile.ReadLine()
+            strDepartment = Time2PayDataFile.ReadLine()
+            strHoursWorked = Time2PayDataFile.ReadLine()
+            strHourlyPayRate = Time2PayDataFile.ReadLine()
+
+            If strEmployeeName.Contains(strEmployeeDisplay) = True Then
+                lstDisplay.Items.Add("Employee Name: " & strEmployeeName)
+                lstDisplay.Items.Add("Department: " & strDepartment)
+                lstDisplay.Items.Add("Hours Worked: " & strHoursWorked)
+                lstDisplay.Items.Add("Hourly Pay Rate: " & FormatCurrency(strHourlyPayRate))
+                Return
+            End If
+        Loop
+
+        If lstDisplay.Items.Count < 1 Then
+            MessageBox.Show("We do not employ this person.")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstDisplay.Items.Clear()
+        txtEmployeeName.Clear()
+        txtDepartment.Clear()
+        txtHoursWorked.Clear()
+        txtHourlyPayRate.Clear()
+    End Sub
+End Class
